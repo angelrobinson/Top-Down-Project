00e2bd2b4bb52a5c6368e56106391fa30a8d7f4a
Angela Robinson <angelrobinson13@outlook.com>
2019-06-16 21:34:18 -0400

    Final Push
    Final Push

--- Top-Down-Project/Assets/Scripts/PlayerController.cs
+++ Top-Down-Project/Assets/Scripts/PlayerController.cs
@@ -32,7 +32,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FindMouseLocation();
+        //FindMouseLocation();
         
         Movement();
     }
